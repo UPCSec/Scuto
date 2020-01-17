7c67eecd518dbd010287241a1eed53086a3e8db8
Semesse <Semperia@outlook.com>
2020-01-17 23:20:39 +0800

    added docker utility and search image from dockerhub

--- scuto/docker/__init__.py
+++ scuto/docker/__init__.py
@@ -1,1 +1,9 @@
 import docker
+from scuto.config import config
+from .dockerhub import search_image
+
+
+client = docker.DockerClient(**config['Docker'])
+
+async def pull_image(repo, tag='latest'):
+    client.images.pull(name, tag)
---
+++ scuto/docker/dockerhub.py
@@ -0,0 +1,16 @@
+import aiohttp
+
+async def search_image(repo, page, page_size):
+    async with aiohttp.ClientSession() as session:
+        url = 'https://hub.docker.com/api/content/v1/products/search'
+        params = {
+            'q': repo,
+            'type': 'image',
+            'page': page,
+            'page_size': page_size
+        }
+        headers = {
+            'Search-Version': 'v3'
+        }
+        async with session.get(url, params=params, headers=headers) as resp:
+            return resp.json()
